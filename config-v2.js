599d211a2355e81aaca6bec09da5bf7224e6256e
isurucm1 <150244155+isurucm1@users.noreply.github.com>
2024-11-26 19:09:39 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "94766632281"
+const SESSION_ID = "PRABATH-MD~BfVXwa7J#FvKcZD1RJqspCyQXHuFAJQqWz-JxxMEd3AVtOFV1kHM"
 
 
 module.exports = {
